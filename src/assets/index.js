d89fab6627be2932bae33b082aba67715689aa8e
unknown <joheandroid@gmail.com>
2023-11-21 16:56:35 -0300

    fix

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+export { default as header } from './header.png';
+export { default as abejaazul } from './abejaazul.png';
+export { default as ulmonivel } from './ulmonivel.png';
+export { default as hulmoorcanico } from './hulmoorcanico.png';
+export { default as ulmo1 } from './1.png';
