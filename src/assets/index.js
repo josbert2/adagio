6659634732ce0840ef344023d915d25d470b0f12
unknown <joheandroid@gmail.com>
2023-11-22 17:52:17 -0300

    fix

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,7 +3,23 @@ export { default as abejaazul } from './abejaazul.png';
 export { default as ulmonivel } from './ulmonivel.png';
 export { default as hulmoorcanico } from './hulmoorcanico.png';
 export { default as ulmo1 } from './1.png';
+export { default as ulmo2 } from './2.png';
+export { default as ulmo3 } from './3.png';
+export { default as ulmo4 } from './4.png';
 
 
 export { default as hiervaazul } from './hiervaazul.png';
 export { default as hiervaazulabejita } from './hiervaazulabejita.png';
+export { default as titulo } from './titulo.png';
+export { default as abeja2 } from './abeja2.png';
+export { default as abeja3 } from './3abejashierva.png';
+export { default as abeja5 } from './abeja4.png';
+
+export { default as abeja4 } from './abeja3.png';
+export { default as abejaorcanica } from './abejaorcanica.png';
+export { default as imagen3 } from './imagen3.png';
+export { default as imagen4 } from './imagen4.png';
+
+export { default as quil } from './quil.png';
+
+export { default as artisan } from './artisan.png';
