558412dff8e4d8328c98f9999b800694e8e1be6c
unknown <joheandroid@gmail.com>
2023-11-22 00:55:43 -0300

    upload to prod

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,3 +3,7 @@ export { default as abejaazul } from './abejaazul.png';
 export { default as ulmonivel } from './ulmonivel.png';
 export { default as hulmoorcanico } from './hulmoorcanico.png';
 export { default as ulmo1 } from './1.png';
+
+
+export { default as hiervaazul } from './hiervaazul.png';
+export { default as hiervaazulabejita } from './hiervaazulabejita.png';
